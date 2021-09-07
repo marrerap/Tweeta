bbb9d83e40aa0ad60523c8d5224436e44b6865fe
marrerap <andrew.marrero@outlook.com>
2021-09-07 11:37:29 -0500

    added regitration route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,44 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const db = require("../models");
+const bcrypt = require("bcrypt");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//  Register new User
+router.post("/", function (req, res, next) {
+  //  take username, password
+  if (!req.body || !req.body.username || !req.body.password) {
+    res.status(422).json({
+      error: "Must Include username and password",
+    });
+    return
+  }
+  //  check if username is unique
+  db.User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  }).then((user) => {
+    if (user) {
+      res.status(400).json({
+        error: "username is already in use",
+      });
+      return;
+    } // hash password store in db
+    bcrypt.hash(req.body.password, 10).then((hash) => {
+      //store user details
+      db.User.create({
+        username: req.body.username,
+        password: hash,
+      }).then((user) => {
+        //respond with success
+        res.status(201).json({
+          success: "Successfully Registered",
+        });
+      });
+    });
+  });
+
+  // respond with success/error
 });
 
 module.exports = router;
